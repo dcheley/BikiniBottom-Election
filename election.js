80591acf168353df0767d6fb887f49b27ce3f9ad
Daniel Cheley <danielcheley@hotmail.com>
2016-12-04 22:04:14 -0500

    Adding vote form to li elements

--- election.js
+++ election.js
@@ -9,13 +9,14 @@ $(document).ready(function() {
       console.log(responseData.candidates[i].id);
       console.log(responseData.candidates[i].name);
       console.log(responseData.candidates[i].votes);
-      var ulItem = document.createElement('li');
+      var ulItem = $('<li>');
       var id = responseData.candidates[i].id;
       var name = responseData.candidates[i].name;
       var votes = responseData.candidates[i].votes;
 
-      ulItem.append(name + " has " + votes + " votes!");
 
+      ulItem.append(name + " has " + votes + " votes!");
+      ulItem.append("<form class='voteform' method='POST' action='https://bb-election-api.herokuapp.com/vote'><input name='name' type='hidden' value=" + responseData.candidates[i].name + "><input id='submitid' type=\"submit\" value=\"Submit\"> </form><br>")
       $('#candidates').append(ulItem);
     };
   });
